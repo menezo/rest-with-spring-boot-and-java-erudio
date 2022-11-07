63cccf3284be3b9f3a6c0ede377cc96051272503
Menezo <joaovitormenezesm12@gmail.com>
2022-11-07 17:35:42 -0300

    Migration with flyway

---
+++ 05_FirstSepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+	(1,'Belo Horizonte','Joao','Male','Miranda'),
+	(2,'Rio de Janeiro','Maria Alice','Female','Menezes'),
+	(4,'Belo Horizonte','Edna','Female','Reis'),
+	(5,'Belo Horizonte','Lindaura','Female','Reis'),
+	(7,'Belo Horizonte','Joao','Male','Vitor Vidal'),
+	(11,'Belo Horizonte','Raphael','Male','Sena'),
+	(12,'Smiljan - Croatia','Nikola','Male','Tesla');
